c186c7ce0e4a7fc872e4b9bb906ec1a2d074d7bf
Tushar Mehta <tsrmehta@gmail.com>
2024-11-19 21:56:35 +0530

    updated mount logic for products

--- products/src/bootsrap.js
+++ products/src/bootsrap.js
@@ -1,10 +1,35 @@
 import faker from "faker";
 
-let products = "";
+// Context/Situation #1
+// We are running this file in development in isolation
+// we are using our local index.html which definitely has an element with an id of 'dev-products'
+// we want to immediately render our app into that element
 
-for(let i =0;i<5;i++){
+const mount = (el) => {
+  let products = "";
+
+  for (let i = 0; i < 5; i++) {
     const name = faker.commerce.productName();
-    products+= `<div>${name}</div>`;
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+//   ReactDOM.render(<App/>, el);
+};
+if(process.env.NODE_ENV === 'development'){
+    const el = document.querySelector("#dev-products");
+
+    // assuming our container doesn't have an element with id "dev-products"
+    if(el){
+        mount(el);
+    }
+
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+// Context/Situation 2
+// We are running this file in development or production
+// through the container app
+// No guarantee that an element with an id of "dev-products"
+// We do not want try to immediately render the app
+
+export {mount};
